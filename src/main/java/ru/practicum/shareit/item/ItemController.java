330f3d8e86314894ce4e77c3bc8b4c2a151903b9
Alexander <ssddds73@gmail.com>
2023-04-10 00:15:23 +0300

    <fix>: испралены ошибки

--- src/main/java/ru/practicum/shareit/item/ItemController.java
+++ src/main/java/ru/practicum/shareit/item/ItemController.java
@@ -2,6 +2,7 @@ package ru.practicum.shareit.item;
 
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+import ru.practicum.shareit.item.dto.CommentDto;
 import ru.practicum.shareit.item.dto.ItemAllDto;
 import ru.practicum.shareit.item.dto.ItemDto;
 import ru.practicum.shareit.item.service.ItemService;
@@ -44,6 +45,14 @@ public class ItemController {
     @GetMapping("/search")
     public List<ItemDto> search(@RequestParam String text) {
         return itemService.getByText(text.toLowerCase());
+    }
+
+    @PostMapping("{itemId}/comment")
+    public CommentDto createComment(@RequestBody CommentDto commentDto,
+                                    @PathVariable Long itemId,
+                                    @RequestHeader(value = "X-Sharer-User-Id", required = false)
+                                    Long userId) {
+        return itemService.createComment(commentDto, itemId, userId);
     }
 
 }
