8471382856ab68c118239736c71f53ec27fdfd88
Alexander <ssddds73@gmail.com>
2023-04-26 18:02:47 +0300

    <fecth> Реализовано ТЗ 16

---
+++ gateway/src/main/java/ru/practicum/shareit/user/controller/UserController.java
@@ -0,0 +1,51 @@
+package ru.practicum.shareit.user.controller;
+
+import org.springframework.validation.annotation.Validated;
+import ru.practicum.shareit.user.dto.Create;
+import ru.practicum.shareit.user.dto.Update;
+import ru.practicum.shareit.user.client.UserClient;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.http.ResponseEntity;
+import ru.practicum.shareit.user.dto.UserDto;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.Valid;
+
+@Slf4j
+@Validated
+@RestController
+@AllArgsConstructor
+@RequestMapping(path = "/users")
+public class UserController {
+    private final UserClient userClient;
+
+    @Validated(Create.class)
+    @PostMapping()
+    public ResponseEntity<Object> createUser(@RequestBody @Valid UserDto userDto) {
+        return userClient.createUser(userDto);
+    }
+
+    @Validated(Update.class)
+    @PatchMapping("/{userId}")
+    public ResponseEntity<Object> updateUser(@RequestBody @Valid UserDto userDto,
+                                             @PathVariable Long userId) {
+        return userClient.updateUser(userDto, userId);
+    }
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<Object> getUser(@NotNull @PathVariable Long userId) {
+        return userClient.getUser(userId);
+    }
+
+    @DeleteMapping("/{userId}")
+    public void deleteUser(@PathVariable Long userId) {
+        userClient.deleteUser(userId);
+    }
+
+    @GetMapping()
+    public ResponseEntity<Object> getAllUsers() {
+        return userClient.getUsers();
+    }
+}
